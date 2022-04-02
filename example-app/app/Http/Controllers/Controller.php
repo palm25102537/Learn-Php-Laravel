ae14bcc106c2c713c7bbbe5833a7a47a652945a0
Wasin <xelticross@gmail.com>
2022-04-02 08:45:36 +0700

    create controller

--- example-app/app/Http/Controllers/Controller.php
+++ example-app/app/Http/Controllers/Controller.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; 
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests; //import specific class or name space
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
